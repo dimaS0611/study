d80506a535a0f1cce88c5b4cd2ce497c5c81cff4
dimaS0611 <75484143+dimaS0611@users.noreply.github.com>
2020-12-04 17:17:36 +0300

    Add files via upload

---
+++ C#/json/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Json
+{
+    public class Author
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string born { get; set; }
+        public string died { get; set; }
+        public string title { get; set; }
+        public string type { get; set; }
+        public int year { get; set; }
+
+        public override string ToString()
+        {
+            return("ID: " + id.ToString() + "\n" + "Имя: " + Name.ToString() + "\n" + "Дата рождения: " + born.ToString() + "\n"  
+                + "Дата смерти: " + died.ToString() + "\n" + "Название произведения: " + title.ToString() + "\n" 
+                + "Тип произведения :"+ type.ToString() + "\n" + "Год издания: " + year.ToString() + "\n");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonString = File.ReadAllText("data.json");
+            
+            if(jsonString.Length > 0)
+            {
+                var authors = JsonConvert.DeserializeObject<List<Author>>(jsonString);
+
+                foreach (var author in authors)
+                {
+                    Console.WriteLine(author.ToString());
+                }
+            }
+        }
+    }
+}
